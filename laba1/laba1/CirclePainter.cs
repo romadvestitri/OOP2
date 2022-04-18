4474913779a6bc65133f21ebbae39d9a7ea49541
romadvestitri <79081931+romadvestitri@users.noreply.github.com>
2022-04-18 04:42:06 +0300

    Add files via upload

---
+++ laba1/laba1/CirclePainter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    public class CirclePainter : EllipsePainter
+    {
+        public CirclePainter(PointF point, float radius)
+        {
+            Pointst = point;
+            RadiusX = radius;
+            RadiusY = radius;
+        }
+     
+    }
+}
---
+++ laba1/laba1/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace laba1
+{
+    public abstract class Figure
+    {
+        
+
+        
+        public virtual PointF PointStart { get; set; }
+
+       // public abstract void Draw(Graphics painter);
+
+
+        
+    }
+}
